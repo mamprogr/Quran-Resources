edcd6cea3573d0dcfc903e0a0bb4b23734ceec0c
MAMProgr <mamprogr@gmail.com>
2012-03-06 15:18:17 +0200

    repaire problems in cotroller with new model !

--- application/controllers/quran_text.php
+++ application/controllers/quran_text.php
@@ -3,39 +3,18 @@
 class Quran_text extends CI_Controller {
     
     private $SuraAya_Delimiter = '-';
+    private $SuraAya_SID = 3; //$SuraAya_SegmentID;
+    private $Sura_SID = 3; //$SuraAya_SegmentID;
     
     function __construct(){
         parent::__construct();
-        $this->load->model('model_quran_text','data');
+        $this->load->model('Model_quran_text','data')  ;
         $this->data->set_QuranType('quran_simple_min');
-        //$this->data->Aya_Style = '<div class=aya>$1</div>';
-        $this->data->Aya_Style = ' $1 ';
-        $this->data->Ayas_Glue = '';
-        $this->data->Sura_Style = '<div class=aya>$1</div>';
-        $this->data->Suras_Glue = '<br />';
-        $this->data->AyaNum_Style = '<span class=ayaNum> ﴿$1﴾ </span>';
-        $this->data->WaqfMark_Style = '<span class="sign">&nbsp;$1</span>';
+        $this->data->set_Basmalah(FALSE);
     }
     
     public function index()
     {
-        $this->Aya(1);
-        //echo $this->quran_data->get_Aya(2);
-        //echo $this->data->get_Aya(1,1);
-        //echo $this->quran_data->get_Aya(2,1,1);
-
-        //echo $this->quran_data->get_SuraInfo(2,'rukus');
-                
-        //$SuraAya = $this->quran_data->get_JuzInfo(70,5);
-        //$SuraAya = $this->quran_data->get_HizbInfo(70,5);
-        /*
-            $SuraAya = $this->quran_data->get_PageInfo(70,5);
-            var_dump($SuraAya);
-        */
-        //var_dump($this->quran_data->Is_Sajda(17,109));
-        //$this->data->set_QuranType('quran_simple_min');
-        //$this->data->ShowBasmalah = FALSE;
-        //echo $this->data->get_Sura(2);
         //$this->load->view('welcome_message');
     }
 
@@ -49,92 +28,73 @@ class Quran_text extends CI_Controller {
         return $Index;
     }
     
-    function Aya($Index){
-        $text = NULL;
+    function Aya(){
         
-        $this->data->Aya_Style = '<div class=aya>$1</div>';
+        if($this->uri->segment($this->SuraAya_SID) !== FALSE)
+            $Index = $this->_Segment2AyaIndex($this->SuraAya_SID);
+        else return;
         
-        if($this->uri->segment(3) !== FALSE)
-            $Index = $this->_Segment2AyaIndex(3);
-        elseif(!isset($Index))return;
+        if($this->data->Is_AyaByIndex($Index))
+            $Aya = $this->data->get_AyaByIndex($Index);
+        else return;
         
+        $data['Aya'] = $Aya;
         
-        if($this->data->Is_AyaIndex($Index))
-            $text = $this->data->get_Aya($Index);
-        
-        /** Old Way ..
-        $Aya = $Sura = $text = NULL;
-        if($this->uri->segment(3) !== FALSE){
-            if($this->data->Is_AyaIndex($this->uri->segment(3)))
-                $SuraAya = $this->data->Index2SuraAya($this->uri->segment(3));
-            else
-                $SuraAya = explode($this->SuraAya_Delimiter, $this->uri->segment(3));
-
-            if(count($SuraAya) == 2){
-                $Sura = $SuraAya[0];
-                $Aya  = $SuraAya[1];
-            } // else = NULL; ^_^
-        }
-        if ($this->data->Is_Sura($Sura) && $this->data->Is_Aya($Sura,$Aya))
-            $text = $this->data->get_Aya($Sura,$Aya);
-        **/
-        
-        // else = NULL; ^_^
-        $data['data'] = $text;
-        $this->load->view('output',$data);
+        $this->load->view('aya',$data);
     }
     
     function Ayas(){
-        $text = NULL;
         
-        $this->data->Aya_Style = '<div class=aya>$1</div>';
+        if($this->uri->segment($this->SuraAya_SID) !== FALSE || $this->uri->segment($this->SuraAya_SID+1) !== FALSE){
+            $From_Index = $this->_Segment2AyaIndex($this->SuraAya_SID);
+            $To_Index   = $this->_Segment2AyaIndex($this->SuraAya_SID+1);
+        }else return;
         
-        if($this->uri->segment(3) === FALSE || $this->uri->segment(4) === FALSE) return;
+//        $From > $To ? $From ^= $To ^= $From ^= $To:1; //By MAMProgr (:^_^:);
+        $From_Index > $To_Index ? list($From_Index, $To_Index) = array($To_Index, $From_Index):1; // Swap ^_^;
         
-        $From = $this->_Segment2AyaIndex(3);
-        $To   = $this->_Segment2AyaIndex(4);
+        if($this->data->Is_AyaByIndex($From_Index) && $this->data->Is_AyaByIndex($To_Index))
+            $Ayas = $this->data->get_AyasByIndex($From_Index,$To_Index);
+        else return;
         
-        //$From > $To ? $From ^= $To ^= $From ^= $To:1; //By MAMProgr (:^_^:);
-        $From > $To ? list($From, $To) = array($To, $From):1; // Swap ^_^;
+        $data['Ayas'] = $Ayas;
         
-        if($this->data->Is_AyaIndex($To) && $this->data->Is_AyaIndex($To))
-            $text = $this->data->get_Ayas($From,$To);
-        // else = NULL; ^_^
+        $this->load->view('ayas',$data);
         
-        $data['data'] = $text;
-        $this->load->view('output',$data);
     }
     
     function Sura(){
-        $Sura = $text = NULL;
         
-        if($this->uri->segment(3) !== FALSE){
-            $Sura = $this->uri->segment(3);
-            if ($this->data->Is_Sura($Sura))
-                $text = $this->data->get_Sura($Sura);
-        }
-        // else = NULL; ^_^
+        if($this->uri->segment($this->Sura_SID) !== FALSE)
+            $Index = $this->uri->segment($this->Sura_SID);
+        else return;
         
-        $data['data'] = $text;
-        $this->load->view('output',$data);
+        if($this->data->Is_Sura($Index))
+            $Sura = $this->data->get_Sura($Index);
+        else return;
+        
+        $data['Sura']     = $Sura;
+        $data['SuraInfo'] = $this->data->get_SuraInfo($Index);
+        
+        $this->load->view('sura',$data);
     }
     
     function Suras(){
-        $Sura = $text = NULL;
         
-        if($this->uri->segment(3) !== FALSE && $this->uri->segment(4) !== FALSE){
-            $From = $this->uri->segment(3);
-            $To = $this->uri->segment(4);
-            
-            if ($this->data->Is_Sura($From) && $this->data->Is_Sura($To))
-                $text = $this->data->get_Suras($From,$To);
-        }
-        // else = NULL; ^_^
+        if($this->uri->segment($this->Sura_SID) !== FALSE || $this->uri->segment($this->Sura_SID+1) !== FALSE){
+            $From_Sura = $this->uri->segment($this->Sura_SID);
+            $To_Sura   = $this->uri->segment($this->Sura_SID+1);
+        }else return;
         
-        $data['data'] = $text;
-        $this->load->view('output',$data);
+        if($this->data->Is_Sura($From_Sura) && $this->data->Is_Sura($To_Sura))
+            $Suras = $this->data->get_Suras($From_Sura,$To_Sura);
+        else return;
+        
+        $data['Suras']     = $Suras;
+        $data['SurasInfo'] = $this->data->get_SurasInfo($From_Sura,$To_Sura);
+        
+        $this->load->view('suras',$data);
     }
-}
 
 /* End of file quran_text.php */
 /* Location: ./application/controllers/quran_text.php */
