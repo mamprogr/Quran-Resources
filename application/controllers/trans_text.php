871a0a341906558f1366ce08b6a41122b2487433
MAMProgr <mamprogr@gmail.com>
2012-03-10 22:47:54 +0200

    fix error :)

--- application/controllers/trans_text.php
+++ application/controllers/trans_text.php
@@ -5,8 +5,8 @@ class Trans_text extends CI_Controller {
     private $SuraAya_Delimiter  = '-';
     private $SuraAya_SID        = 5; //$SuraAya_SegmentID;
     private $Sura_SID           = 5; //$SuraAya_SegmentID;
-    private $Trans_SID          = 4; //$Trans_SegmentID;
-    private $ViewType_SID       = 3; //$ViewType_SegmentID;
+    private $Trans_SID          = 3; //$Trans_SegmentID;
+    private $ViewType_SID       = 4; //$ViewType_SegmentID;
     private $ViewType           = 'html';
     
     function __construct(){
