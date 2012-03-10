818a1a614ca646b045c939837bf664ce9e58d815
MAMProgr <mamprogr@gmail.com>
2012-03-10 22:36:37 +0200

    add xml view for translation.

---
+++ application/views/trans_text/xml/ayas.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Content-type: text/xml');
+
+$xml_Ayas = new SimpleXMLElement('<Ayas/>');
+//$Ayas->addAttribute('type', 'documentary');
+
+foreach ($Ayas as $Aya) {
+	$xml_Aya = $xml_Ayas->addChild('Aya',$Aya['text']);
+    $xml_Aya->addAttribute('index', $Aya['index']);
+    $xml_Aya->addAttribute('sura', $Aya['sura']);
+    $xml_Aya->addAttribute('aya', $Aya['aya']);
+}
+echo $xml_Ayas->asXML();
+
+?>
